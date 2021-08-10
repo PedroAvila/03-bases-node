0df2f42ab955d7cf942b10ca3bacaab4625e04e1
PedroAvila <minimainor_austin@hotmail.com>
2021-08-10 17:58:24 -0500

    Requerir paquetes - require

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
+const fs = require('fs');
 
 console.clear();
 console.log('==================================');
 console.log('Tabla del: 5 ');
 console.log('==================================');
 
-const base = 5;
+const base = 3;
+let salida = '';
 
 for (let i = 1; i <= 10; i++) {
-    console.log( `${ base } x ${ i } = ${ base * i }` );
-    
+    salida +=  `${ base } x ${ i } = ${ base * i }\n`;  
 }
+
+console.log(salida);
+
+fs.writeFile( `tabla-${ base }.txt`, salida, (err)=> {
+    if (err) throw err;
+
+    console.log(`tabla-${ base }.txt creado`);
+} )
 
 
 
