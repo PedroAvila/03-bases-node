6369db3ff9db8bf1daff9c6afd12576e007d9a99
PedroAvila <minimainor_austin@hotmail.com>
2021-08-17 10:54:01 -0500

    Yargs

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 
 const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('yargs').argv;
+
+
 
 console.clear();
 
+console.log( process.argv );
+console.log( argv );
 
+console.log( 'base: yargs', argv.base );
 
-const [ , , arg3 = 'base=5' ] = process.argv;
-const [ , base = 5 ] = arg3.split( '=' ); 
+// const [ , , arg3 = 'base=5' ] = process.argv;
+// const [ , base = 5 ] = arg3.split( '=' ); 
 
 
 //const base = 5;
 
-crearArchivo( base )
-    .then( nombreArchivo => console.log( nombreArchivo, 'creado' ) )
-    .catch( err => console.log( err ) );
+// crearArchivo( base )
+//     .then( nombreArchivo => console.log( nombreArchivo, 'creado' ) )
+//     .catch( err => console.log( err ) );
 
 
 
